c4d95b63d5fc8845de98ce5f2db28f9d58b1739c
Wilson-Chu <wilson1.chu@gmail.com>
2022-08-17 12:47:12 -0400

    import tailwind-react native classnames and implemented into home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "twrnc";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <Text style={[tw`text-red-500 p-10`]}>I am the HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+  },
+});
